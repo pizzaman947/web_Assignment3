bcbf8e602b0a1ba332123926dd2fe2118c1ca270
user <user@example.com>
2026-01-14 15:37:50 +0500

    fix edge cases

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const blogRoutes = require('./blog_routes');
+const blogRoutes = require('./routes/blog_routes');
 const dotenv = require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(express.static(__dirname)); 
+app.use(express.static(__dirname + '/static')); 
 
 const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI)
@@ -13,7 +13,7 @@ mongoose.connect(mongoURI)
     .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/static/index.html");
 });
 app.use('/blogs', blogRoutes);
 
